3cb2628e0f13b94e1bd8e14a5367d766580de7b1
Andrew Lien <andrew.lien@thecodingblog.org>
2016-01-30 10:58:10 -0800

    TODO - Max - bus service

--- slugloop/www/js/services.js
+++ slugloop/www/js/services.js
@@ -3,7 +3,29 @@ angular.module('app.services', [])
 .factory('BlankFactory', [function(){
 
 }])
+.service('BusService',[function(){
 
+  var row_16 = [];
+  var row_19 = [];
+  return {
+    get_bus_16: function() {
+            return row_16;
+        },
+     get_bus_19: function(){
+     	return row_19;
+     },
+    set_bus_16: function(value){
+    	console.log("I am pushing a value");
+            row_16.push(value);
+    },
+    set_bus_19: function (value){
+    	row_19.push(value);
+    }
+
+
+      }
+
+}])
 .service('BlankService', [function(){
 
 }]);
