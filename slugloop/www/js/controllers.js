3d088822d46512481306be43dac7c09716895a12
Andrew Lien <andrew.lien@thecodingblog.org>
2016-01-30 11:12:38 -0800

    Added location modal

--- slugloop/www/js/controllers.js
+++ slugloop/www/js/controllers.js
@@ -1,7 +1,37 @@
 angular.module('app.controllers', [])
   
-.controller('loopCtrl', function($scope) {
+.controller('loopCtrl', function($scope,$ionicModal) {
+  $ionicModal.fromTemplateUrl('templates/location_modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+      $scope.modal_location = modal;
+  });
 
+   $scope.openModal = function() {
+   console.log("lol");
+     $scope.modal_location.show();
+   }
+
+    //close our modal
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal_term.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+
+  
 })
    
 .controller('feedCtrl', function($scope, $http) {
